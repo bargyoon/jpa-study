2cc4cc3523c2da3029f31cb7a8e29cb6b0803cb9
bargyoon <bargyoon@gmail.com>
2021-11-01 22:19:29 +0900

    [jpa] lombok 수정

--- src/com/kh/bookmanager/book/Book.java
+++ src/com/kh/bookmanager/book/Book.java
@@ -1,12 +1,10 @@
 package com.kh.bookmanager.book;
 
-import java.sql.Date;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.hibernate.annotations.DynamicInsert;
--- src/com/kh/bookmanager/member/Member.java
+++ src/com/kh/bookmanager/member/Member.java
@@ -1,6 +1,5 @@
 package com.kh.bookmanager.member;
 
-import java.sql.Date;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
